137e17fd203a04c5f1d12283195f25bb9e488806
Lucky Coder <47534548+Muxsin@users.noreply.github.com>
2025-02-07 02:59:29 +0700

    Created service backend (#1)
    * Created service backend
    
    * Fixed db connection problem

---
+++ backend/services/ping_service.go
@@ -0,0 +1,22 @@
+package services
+
+import (
+	"monitoring-backend/models"
+	"monitoring-backend/repositories"
+)
+
+type PingService struct {
+	repo *repositories.PingRepository
+}
+
+func NewPingService(repo *repositories.PingRepository) *PingService {
+	return &PingService{repo: repo}
+}
+
+func (s *PingService) GetAllPings() ([]models.Ping, error) {
+	return s.repo.GetAll()
+}
+
+func (s *PingService) CreatePing(result models.Ping) error {
+	return s.repo.Create(result)
+}
